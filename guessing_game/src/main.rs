bde8d337b9df27995014a6fecede407858082d06
Eduardo C. Flores Ambrosio <efloresambrosio@gmail.com>
2020-02-20 13:23:14 -0600

    new: comparison and game loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,19 +1,31 @@
 // a single line comment :D
 // importing the 'io' library of the standard 'std' library.
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 // the entry point for our rust program
 fn main() {
     println!("Guess the number!");
-    println!("Input your guess.");
+    loop {
+        println!("Input your guess.");
+        let secret_number = rand::thread_rng().gen_range(1, 101);
+        // It creates a mutable 'mut' string
+        let mut guess = String::new();
 
-    // It creates a mutable 'mut' string
-    let mut guess  = String::new();
+        // Reads the user input
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line :(");
 
-    // Reads the user input
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line :(");
+        let guess: u32 = guess.trim().parse().expect(" Please type a number");
 
-    // Prints the user input
-    println!("Your guessed: {}", guess);
+        // Prints the user input
+        println!("Your guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("To small!"),
+            Ordering::Equal => println!("You win!"),
+            Ordering::Greater => println!("To big!"),
+        }
+    }
 }
