526346de0f3d9bde77802e672f409ed1cacab8b2
Eduardo Flores <efloresambrosio@gmail.com>
2022-05-09 23:34:01 -0500

    new: variable shadowing

--- variables-scope-shadowing/src/main.rs
+++ variables-scope-shadowing/src/main.rs
@@ -16,4 +16,22 @@ fn main() {
     // FIXME ^ Comment out this line
 
     println!("outer long: {}", long_lived_binding);
+
+    // Shadowing
+
+    let shadowed_binding = 1;
+
+    {
+        println!("before being shadowed: {}", shadowed_binding);
+
+        // This binding *shadows* the outer one
+        let shadowed_binding = "abc";
+
+        println!("shadowed in inner block: {}", shadowed_binding);
+    }
+    println!("outside inner block: {}", shadowed_binding);
+
+    // This binding *shadows* the previous binding
+    let shadowed_binding = 2;
+    println!("shadowed in outer block: {}", shadowed_binding);
 }
