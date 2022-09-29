c003fc1c8cdffbd18919956f780c6e732953ee73
Eduardo Flores <efloresambrosio@gmail.com>
2022-09-29 05:47:09 +0000

    new: now db is created ready to run the app

---
+++ fr-url-shortener/init_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.urls (id, key, secret_key, target_url, is_active, clicks, created_at, updated_at) VALUES (1, 'a', 'aa', 'aaaa', true, 0, '2022-08-25 00:31:22.000000', '2022-08-25 05:31:24.097450');
+INSERT INTO public.urls (id, key, secret_key, target_url, is_active, clicks, created_at, updated_at) VALUES (2, 'b', 'bb', 'bbbb', true, 0, '2022-08-25 00:31:22.000000', '2022-08-25 05:31:24.097450');
+INSERT INTO public.urls (id, key, secret_key, target_url, is_active, clicks, created_at, updated_at) VALUES (3, 'c', 'cc', 'cccc', true, 0, '2022-08-25 00:31:22.000000', '2022-08-25 05:31:24.097450');
+INSERT INTO public.urls (id, key, secret_key, target_url, is_active, clicks, created_at, updated_at) VALUES (4, 'd', 'dd', 'ddd', true, 0, '2022-08-25 00:31:22.000000', '2022-08-25 05:31:24.097450');
+INSERT INTO public.urls (id, key, secret_key, target_url, is_active, clicks, created_at, updated_at) VALUES (5, 'e', 'ee', 'eee', true, 0, '2022-08-25 00:31:22.000000', '2022-08-25 05:31:24.097450');
