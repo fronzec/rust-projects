c028933e6f2a53f0651fbcd6e526cf87e6519a5f
Eduardo Flores <efloresambrosio@gmail.com>
2023-11-09 01:51:33 -0600

    Implement stdin character reading in main
    A change has been made in the function main to accept character type inputs from the standard input. The program will continue to print the characters as they come in and it will halt execution when it encounters the character 'q'.

--- text-editor/src/main.rs
+++ text-editor/src/main.rs
@@ -1,3 +1,14 @@
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
+    // Read input from stdin
+    for b in io::stdin().bytes() {
+        let c = b.unwrap() as char;
+        println!("{}", c);
+        // Note: If we read a character q then exit the program
+        if 'q' == c {
+            break;
+        }
+    }
+
 }
