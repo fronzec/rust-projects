1072f030b47a0bcfa548b1a2a09aa9bcb7189f44
Eduardo Flores <efloresambrosio@gmail.com>
2023-12-26 20:19:28 -0600

    new: error handling

--- text-editor/src/main.rs
+++ text-editor/src/main.rs
@@ -6,20 +6,30 @@ fn to_ctrl_byte(c: char) -> u8 {
     byte & 0b0001_1111
 }
 
+fn die( e: std::io::Error) {
+    panic!(e)
+}
+
 fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
     // Read input from stdin
     for b in io::stdin().bytes() {
-        let b = b.unwrap();
-        let c = b as char;
-        if c.is_control() {
-            println!("{:?} \r", b);
-        } else {
-            println!("{:?} ({})\r", b, c);
+        match b {
+            Ok(b) => {
+                let b = b.unwrap();
+                let c = b as char;
+                if c.is_control() {
+                    println!("{:?} \r", b);
+                } else {
+                    println!("{:?} ({})\r", b, c);
+                }
+                if b == to_ctrl_byte('q') {
+                    break;
+                }
+            }
+            Err(err) => die(err)
         }
-        if b == to_ctrl_byte('q') {
-            break;
-        }
+
     }
 
 }
