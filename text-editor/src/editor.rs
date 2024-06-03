b07a436fd14195e701ee816bb81d71c38a90df06
Eduardo Flores <efloresambrosio@gmail.com>
2024-06-02 23:59:27 -0600

    Formatting format code

--- text-editor/src/editor.rs
+++ text-editor/src/editor.rs
@@ -1,21 +1,19 @@
-use std::io::{self, Read};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use std::io::{self, Read};
 
-pub struct Editor {
-
-}
+pub struct Editor {}
 
 impl Editor {
     pub fn default() -> Self {
-        Editor{}
+        Editor {}
     }
-    pub fn run(&self){
+    pub fn run(&self) {
         enable_raw_mode().unwrap();
         for b in io::stdin().bytes() {
             match b {
                 Ok(b) => {
                     let c = b as char;
-                    if c.is_control(){
+                    if c.is_control() {
                         println!("Binary :{0:08b} ASCII:{0:#03} \r", b);
                     } else {
                         println!("Binary :{0:08b} ASCII: {0:#03} Character: {1:#?} \r", b, c);
