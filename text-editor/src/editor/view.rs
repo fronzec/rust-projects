dcbd24a221c690f486e4552447bb79b717ca9d0a
Eduardo Flores <efloresambrosio@gmail.com>
2024-08-16 03:56:30 -0600

    Added simple buffer structure for code editor

--- text-editor/src/editor/view.rs
+++ text-editor/src/editor/view.rs
@@ -1,22 +1,32 @@
 use super::terminal::{Size, Terminal};
 use std::io::Error;
+mod buffer;
+use buffer::Buffer;
 
 const NAME: &str = env!("CARGO_PKG_NAME");
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-pub struct View;
+#[derive(Default)]
+pub struct View {
+    buffer: Buffer
+}
 
 impl View {
-    pub fn render() -> Result<(), Error> {
+    pub fn render(&self) -> Result<(), Error> {
         let Size { height, .. } = Terminal::size()?;
 
         for current_row in 0..height {
+            if let Some(line) = self.buffer.lines.get(current_row) {
+                Terminal::print(line)?;
+                Terminal::print("\r\n")?;
+                continue;
+            }
             Terminal::clear_line()?;
             #[allow(clippy::integer_division)]
             if current_row == height / 3 {
-                Self::draw_welcome_message();
+                Self::draw_welcome_message()?;
             } else {
-                Self::draw_empty_row();
+                Self::draw_empty_row()?;
             }
             if current_row.saturating_add(1) < height {
                 Terminal::print("\r\n")?;
