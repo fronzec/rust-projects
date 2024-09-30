2ef2f3e547ef2e25f11bf6d4791cd3f23dd0a523
Eduardo Flores <efloresambrosio@gmail.com>
2024-09-29 20:41:26 -0600

    Added Rocket as dependency and also add 'hello world' endpoint

--- fr-url-shortener-v2/src/main.rs
+++ fr-url-shortener-v2/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("URL shortener implemented in rust");
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
