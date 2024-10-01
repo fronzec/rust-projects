10a0c20063ac8c7bb0eab2482649c61f47ca15f4
Eduardo Flores <efloresambrosio@gmail.com>
2024-10-01 00:43:09 -0600

    Added some other endpoint to practice endpoints
    - GET ping endpoint
    - GET hello for the given name on the endpoint

--- fr-url-shortener-v2/src/main.rs
+++ fr-url-shortener-v2/src/main.rs
@@ -1,12 +1,25 @@
 #[macro_use]
 extern crate rocket;
 
+use std::fmt::format;
+
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+    "/"
+}
+
+/// This endpoint is used to check the availability and responsiveness of the service. It acts as a health check endpoint.
+#[get("/ping")]
+fn ping() -> &'static str {
+    "pong"
+}
+
+#[get("/hello/<name>")]
+fn hello(name: &str) -> String {
+    format!("Hello, {}!", name)
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, ping, hello])
 }
